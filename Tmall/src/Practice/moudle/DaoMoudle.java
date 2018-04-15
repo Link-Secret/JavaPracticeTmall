362a81a55ea63fb1c225f9e23379dff98ee57497
Link-Secret <zj1078184113@163.com>
2018-04-15 21:30:23 +0800

    PropertyDAO

--- Tmall/src/Practice/moudle/DaoMoudle.java
+++ Tmall/src/Practice/moudle/DaoMoudle.java
@@ -1,5 +1,9 @@
 package Practice.moudle;
 
+import tmall.bean.User;
+
+import java.util.List;
+
 /**
  * \* Created with IntelliJ IDEA.
  * \* User: acer zjl
@@ -7,5 +11,51 @@ package Practice.moudle;
  * \* Description:
  * \
  */
-public class daoMoudle {
+public class DaoMoudle {
+
+    /*增加*/
+    public void add(User bean){
+
+    }
+
+
+    /*2. 删除*/
+
+    public void delete(int id) {
+
+    }
+
+
+    /*3. 修改*/
+
+    public void update(User bean) {
+
+    }
+
+
+    /*4. 根据id获取*/
+
+    public User get(int id) {
+        return null;
+    }
+
+
+    /*5. 分页查询*/
+
+    public List<User> list(int start, int count) {
+        return null;
+    }
+
+    /*6. 查询所有*/
+
+    public List<User> list() {
+        return null;
+    }
+
+
+    /*7. 获取总数*/
+
+    public int getTotal() {
+        return 0;
+    }
 }
--- Tmall/src/Practice/test/Test.java
+++ Tmall/src/Practice/test/Test.java
@@ -1,4 +1,4 @@
-package tmall.test;
+package Practice.test;
 
 /**
  * \* Created with IntelliJ IDEA.
@@ -11,7 +11,29 @@ public class Test {
 
     public static void main(String[] args) {
         String name = "zjl";
-        name = name.substring(0,1)+"*";
+        Test test = new Test();
+        name = test.getAnonymousName(name);
         System.out.println(name);
+    }
+
+    public String getAnonymousName(String name) {
+        /*name为空*/
+        if(null == name)
+            return null;
+        if(name.length() == 1){
+            return "*";
+        }
+        if(name.length() == 2) {
+            return name.substring(0,1)+"*";
+        }
+        /*昵称大于2个的*/
+        else{
+            char[] ch = name.toCharArray();
+            for(int i=1;i<ch.length-1;i++) {
+                ch[i] = '*';
+            }
+            return new String(ch);
+            /*return String.valueOf(ch);*/
+        }
     }
 }
